2807e7739da0f86b0d92958aa966f7a872df9445
s0s <me@s0s.sh>
2024-12-09 15:39:34 -0600

    Strip out Steam platform

--- src/main.rs
+++ src/main.rs
@@ -22,24 +22,10 @@ async fn rocket() -> _ {
         .await
         .expect("Failed to init PgDb pool");
 
-    DB.lock()
-        .await
-        .init_table_platform_steam_users()
-        .await
-        .expect("Failed to init PlatformSteamUsers table");
-
     rocket::build()
         .mount("/v1/", routes![routes::v1::get_status])
         .mount(
             "/v1/platforms/",
             routes![routes::v1::platforms::list::get_list],
-        )
-        .mount(
-            "/v1/platforms/steam/users/",
-            routes![
-                routes::v1::platforms::steam::users::get_users_list,
-                routes::v1::platforms::steam::users::get_users_by_id,
-                routes::v1::platforms::steam::users::get_users_count
-            ],
         )
 }
--- src/routes/v1/platforms/list.rs
+++ src/routes/v1/platforms/list.rs
@@ -2,5 +2,5 @@ use rocket::serde::json::{json, Value};
 
 #[get("/list")]
 pub async fn get_list() -> Value {
-    json!(["steam"])
+    json!([])
 }
